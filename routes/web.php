524f79118d83c7cf01d40ce7a8d57721f0e57149
Anh Van <anhvan@example.com>
2025-06-17 21:27:13 +0700

    Fix: add sqlite support, create DB and migrate

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseFullController;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/courses', [CourseFullController::class, 'index'])->name('courses.index');
 
